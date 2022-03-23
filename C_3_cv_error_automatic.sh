f567e21bbca39a99810e15527097a4ee1ea725d5
Olga Vrou <olgavrou@gmail.com>
2022-03-22 20:57:34 -0400

    add labels to axis's and var rename

--- C_3_cv_error_automatic.sh
+++ C_3_cv_error_automatic.sh
@@ -3,10 +3,10 @@
 rm errors_d_*
 
 for d in $(seq 1 5); do
-	for c in $(seq -20 20); do
-		echo "$d - $c"
-		ABC=$(bc -l <<< "3 ^($c)")
-		OUTPUT=$(./libsvm/svm-train -v 5 -t 1 -d $d -c $ABC abalone.data.libsvm.train.scale | tail -1)
+	for k in $(seq -20 20); do
+		echo "$d - $k"
+		C=$(bc -l <<< "3 ^($k)")
+		OUTPUT=$(./libsvm/svm-train -v 5 -t 1 -d $d -c $C abalone.data.libsvm.train.scale | tail -1)
 		ACC=$(echo $OUTPUT | cut -d"=" -f2 | cut -d"%" -f 1)		
 		ERR=$( bc -l <<< "100 - $ACC")
 		echo $ERR >> errors_d_"$d".dat
--- C_3_cv_error_manual.sh
+++ C_3_cv_error_manual.sh
@@ -3,11 +3,11 @@
 rm manual_test_err_*
 
 for d in $(seq 1 5); do
-	for c in $(seq -20 20); do
+	for k in $(seq -20 20); do
 		for inner_d in $(seq 1 5); do
-			echo "$d - $c"
-			ABC=$(bc -l <<< "3 ^($c)")
-			OUTPUT=$(./libsvm/svm-train -t 1 -d $d -c $ABC abalone.data.libsvm.train.scale_"$inner_d"_rest | tail -1)
+			echo "$d - $k"
+			C=$(bc -l <<< "3 ^($k)")
+			OUTPUT=$(./libsvm/svm-train -t 1 -d $d -c $C abalone.data.libsvm.train.scale_"$inner_d"_rest | tail -1)
 			OUTPUT=$(./libsvm/svm-predict abalone.data.libsvm.train.scale_"$inner_d" abalone.data.libsvm.train.scale_"$inner_d"_rest.model out | tail -1)
 			ACC=$(echo $OUTPUT | cut -d"=" -f2 | cut -d"%" -f 1)		
 			ERR=$( bc -l <<< "100 - $ACC")
